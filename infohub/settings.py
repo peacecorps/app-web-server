299a452bfde38879d77d96bafa60e9107491b209
Yash Shah <yash9414@gmail.com>
2014-08-07 16:59:10 +0000

    Changed settings file

--- infohub/settings.py
+++ infohub/settings.py
@@ -2,7 +2,15 @@
 #Author : Vaibhavi Desai
 #Github username : desaivaibhavi
 #email : ranihaileydesai@gmail.com
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
