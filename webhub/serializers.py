fe846cd1cc033badff0b515d1781c006944e1f05
desaivaibhavi <rani.vhd@gmail.com>
2014-08-15 18:08:08 +0530

    removing the image and link from serializers

--- webhub/serializers.py
+++ webhub/serializers.py
@@ -17,13 +17,13 @@ class PcuserSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('owner', 'title_post', 'description_post', 'link_post', 'created','updated','id','image_post','imageobj_post','id')
+        fields = ('owner', 'title_post', 'description_post', 'created','updated','id')
         
         
 class RevPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = RevPost
-        fields = ('owner_rev_post', 'owner_rev', 'title_post_rev', 'description_post_rev', 'link_post_rev', 'created','id','title_change','description_change','link_change','id')
+        fields = ('owner_rev_post', 'owner_rev', 'title_post_rev', 'description_post_rev', 'created','id','title_change','description_change','id')
         
 #Peacetrack begins here
 
