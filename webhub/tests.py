0f80f4d8ac9afe8cb6485e4db97c6dfa22ca2d01
Irish Medina <irish.m.medina@gmail.com>
2015-06-10 21:23:51 -0500

    Created initial tests for API and moved previous tests to a temp folder since I am not able to run test.py without doing so

--- webhub/tests.py
+++ webhub/tests.py
@@ -1,8 +1,30 @@
-#Version : Phython/Django 2.7.6, PostgreSQL 9.3.4
-#Author : Vaibhavi Desai
-#Github username : desaivaibhavi
-#email : ranihaileydesai@gmail.com
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from webhub.models import Pcuser, Post
 
-from django.test import TestCase
+class PostAPITestCase(APITestCase):
 
-# Create your tests here.
+    #add setUp fixture
+
+    def test_get_posts(self):
+        
+        u1 = User.objects.create_superuser(username='admin', password='password', email='')
+        u1.save()
+
+        o1 = Pcuser(user = u1)
+        o1.save()
+
+        p1 = Post(owner = o1,
+                title_post = "Test",
+                description_post = "Tester")
+        p1.save()
+        
+        self.client.login(username='admin', password='password')
+        #name of viewset is post-detail
+
+        post_id = str(p1.id)
+        url = reverse('post-detail', args=[post_id])
+        response = self.client.get(url)
+        print response
